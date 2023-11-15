09c82259dd2228adf9f6c03eb83e4a948a565093
ArushanSinnadurai <arushan.sinnadurai@gmail.com>
2023-11-14 20:49:39 -0500

    Adding navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import {
   Nav,
   Form,
   FormControl,
+  NavDropdown,
   Button,
   Container,
 } from "react-bootstrap";
@@ -13,33 +14,35 @@ import "./Navbar.css";
 class CustomNavbar extends React.Component {
   render() {
     return (
-      <div className="navbar">
-        <Navbar bg="light" expand="xl">
-          <Container>
-            <Navbar.Brand href="/home">
-              <img className="navBarLogo" src={logo} width="100" height="100" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/home">HOME</Nav.Link>
-                <Nav.Link href="/about">ABOUT US</Nav.Link>
-                <Nav.Link href="/contact">BLOGS</Nav.Link>
-                <Nav.Link href="/contact">CONTACT INFO</Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="/home">
+            <img className="navBarLogo" src={logo} width="100" height="100" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/home">HOME</Nav.Link>
+              <Nav.Link href="/about">ABOUT US</Nav.Link>
+              <Nav.Link href="/contact">BLOGS</Nav.Link>
+              <Nav.Link href="/contact">CONTACT INFO</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }
