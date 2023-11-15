ab8c4b2b95f8ce9ecf34b4375af9ce2b9e87b0ea
ArushanSinnadurai <arushan.sinnadurai@gmail.com>
2023-11-14 20:26:16 -0500

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
 import logo from "./logo512.png";
 import "./Navbar.css";
 
@@ -7,19 +14,28 @@ class CustomNavbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="xl">
           <Container>
             <Navbar.Brand href="/home">
               <img className="navBarLogo" src={logo} width="100" height="100" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link href="/about">HOME</Nav.Link>
+              <Nav className="me-auto">
+                <Nav.Link href="/home">HOME</Nav.Link>
                 <Nav.Link href="/about">ABOUT US</Nav.Link>
                 <Nav.Link href="/contact">BLOGS</Nav.Link>
                 <Nav.Link href="/contact">CONTACT INFO</Nav.Link>
               </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
             </Navbar.Collapse>
           </Container>
         </Navbar>
